238f2eecc76573bb7e07d9e159c23579812d2626
Lyle <5846films@gmail.com>
2017-08-15 21:56:01 -0700

    set up properties and arguments, added pkg.json

---
+++ BasicCard.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+
+exports.BasicCard = function(front, back){
+	this.front = front,
+	this.back = back;
+}
+
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
---
+++ ClozeCard.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+
+exports.ClozeCard = function(text, cloze){
+
+	this.cloze = cloze,
+	this.partial = partial,
+	this.fullText = full text;
+
+}
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); "
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText): "
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
